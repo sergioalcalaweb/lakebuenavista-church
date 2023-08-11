013e8ae7644b8dbe71601f2c98f9e685bc9036da
Sergio Alcala <checo_2k2@hotmail.com>
2023-08-11 18:51:51 -0400

    add try catch

--- src/utils/youtube.ts
+++ src/utils/youtube.ts
@@ -13,9 +13,9 @@ export async function getChannelInfo(maxResults: number = 30): Promise<YoutubeSe
     const response = await fetch(`${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&maxResults=${maxResults}&channelId=${channel}&key=${process.env.YOUTUBE_API_KEY}`);
     const data = await response.json();
     return {
-      videos: data.items.slice(0, 5),
-      conference: data.items.slice(10, 15),
-      concerts: data.items.slice(20, 25),
+      videos: data.items?.slice(0, 5),
+      conference: data.items?.slice(10, 15),
+      concerts: data.items?.slice(20, 25),
     };
   } catch (error) {
     return {
