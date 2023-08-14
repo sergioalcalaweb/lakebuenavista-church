ad7ca8a15de6c3c060a94d8c95a4cb6c51dbaf3b
Sergio Alcala <checo_2k2@hotmail.com>
2023-08-13 22:14:25 -0400

    add time

--- src/components/calendar/monthly-day.tsx
+++ src/components/calendar/monthly-day.tsx
@@ -35,9 +35,9 @@ export function MonthlyDay({ day, locale, events } : MonthlyDayProps) {
             // />
             <li className="py-2" key={index}>
               <h3 className='font-normal'>{item.title}</h3>
-              {/* <div className='flex justify-between items-center mt-4'>
-                <p className="text-gray-900">{format(item.date, 'KK:mm a')}</p>
-              </div> */}
+              <div className='flex justify-between items-center mt-4'>
+                <p className="text-gray-900">{date}</p>
+              </div>
             </li>
           )
         })}
