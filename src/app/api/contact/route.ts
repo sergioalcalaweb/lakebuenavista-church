238acf6c833e9001267289dbb873a93a89dfe482
Sergio Alcala <checo_2k2@hotmail.com>
2024-02-05 21:42:00 -0500

    add email to sender

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,7 +1,8 @@
-import { EMAIL_FROM, RESEND_API_KEY } from "@/config";
+import { EMAIL_FROM, EMAIL_TO, RESEND_API_KEY } from "@/config";
 import { NextResponse } from "next/server"
 import { Resend } from "resend";
 import { EmailContactUser } from '../../../emails/index';
+import EmailThanks from "@/emails/thanks";
 
 // export const runtime = "edge";
 const resend = new Resend(RESEND_API_KEY);
@@ -12,10 +13,18 @@ export async function POST(request: Request) {
     const { email, message, name } = res;
     await resend.emails.send({
       from: EMAIL_FROM || 'onboarding@resend.dev',
-      to: 'checo.ipn@gmail.com',
-      subject: 'Gracias por tu mensaje LBV SDA',
+      to: EMAIL_TO || 'checo.ipn@gmail.com',
+      subject: 'Mensaje recibido desde el sitio web',
       react: EmailContactUser({name, message, email}),
     });
+
+    await resend.emails.send({
+      from: EMAIL_FROM || 'onboarding@resend.dev',
+      to: email,
+      subject: 'Gracias por tu mensaje LBV SDA',
+      react: EmailThanks({name}),
+    });
+
     return NextResponse.json({test: 'success'})
   } catch (error) {
     return NextResponse.json({ error });
