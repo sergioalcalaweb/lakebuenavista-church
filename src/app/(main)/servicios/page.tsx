4a3d734ddbd850cb2e3c735051fc89f703f31011
Sergio Alcala <checo_2k2@hotmail.com>
2024-04-23 19:55:44 -0400

    adding new service section

--- src/app/(main)/servicios/page.tsx
+++ src/app/(main)/servicios/page.tsx
@@ -1,7 +1,15 @@
 import Image from "next/image"
+import {
+  Card,
+  CardHeader,
+  Image as ImageUI,
+  CardFooter,
+  Button,
+} from "@nextui-org/react"
 import SectionName from "@/components/section-name"
 import type { Metadata } from "next"
 import ogImage from "@/app/(main)/opengraph-image.png"
+import Link from "next/link"
 
 export const metadata: Metadata = {
   title: "Servicios",
@@ -42,7 +50,7 @@ const Services = () => {
         <Image
           alt="servicios"
           fill
-          src="/image/servicios.jpg"
+          src="/image/ministerios.jpg"
           loading="eager"
           priority={true}
           className="absolute inset-0"
@@ -54,126 +62,161 @@ const Services = () => {
         />
       }
     >
-      <section className="overflow-hidden sm:grid sm:grid-cols-2 sm:items-center mb-10 md:mb-0">
-        <div className="px-7 md:px-14 py-10 lg:py-24">
-          <div className="mx-auto max-w-xl">
-            <h2 className="title">Servicio de Adoración</h2>
-
-            <p className="text-gray-500 text mt-4">
-              Nos encantaría que se unan a nosotros en nuestro servicio de
-              adoración semanal, que se celebra{" "}
-              <strong>todos los sábados a las 11 am.</strong> Nos reunimos para
-              adorar juntos, aprender más sobre la Palabra de Dios y disfrutar
-              de la comunión con nuestra familia de fe.
-            </p>
-            <p className="hidden text-gray-500 text mt-4 md:block">
-              Nuestro servicio incluye música en vivo, oración, enseñanza
-              bíblica y tiempo para conectarse con otros miembros de la iglesia.
-              Nos esforzamos por crear un ambiente acogedor y amigable para
-              todos, y esperamos que se sientan bienvenidos y cómodos en nuestra
-              comunidad.
-            </p>
-            <p className=" text-gray-500 text mt-4">
-              Si está buscando un lugar para adorar y crecer espiritualmente,
-              ¡nuestra iglesia es el lugar perfecto! Nos encantaría conocerle y
-              darle la bienvenida a nuestra familia de fe. Esperamos verle este{" "}
-              <strong>sábado a las 11 am</strong> en nuestra iglesia Lake Buena
-              Vista.
-            </p>
-          </div>
-        </div>
-
-        <Image
-          src="/image/adoracion1.jpeg"
-          alt="adoracion"
-          width={500}
-          height={500}
-          className="h-full w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-tl-[30px] md:h-[calc(100%_-_4rem)] md:rounded-tl-[60px]"
-        />
-      </section>
-
-      <section className="overflow-hidden sm:grid sm:grid-cols-2 sm:items-center mb-10 md:mb-0">
-        <div className="px-7 md:px-14 py-10 lg:py-24">
-          <div className="mx-auto max-w-xl">
-            <h2 className="title">Escuela Sabática</h2>
-
-            <p className="text-gray-500 mt-4 text">
-              Nos gustaría invitarles a unirse a nosotros todos los{" "}
-              <strong>sábados a las 9:15 am</strong> para un tiempo de
-              aprendizaje bíblico y comunión. Durante la Escuela Sabática,
-              tendremos la oportunidad de estudiar la Palabra de Dios y crecer
-              juntos en nuestra fe. A través de la discusión y el aprendizaje
-              guiado por un maestro, tendremos la oportunidad de profundizar en
-              nuestra comprensión de las Escrituras y cómo aplicarlas a nuestras
-              vidas diarias.
-            </p>
-          </div>
-        </div>
-
-        <Image
-          src="/image/escuela2.jpeg"
-          alt="adoracion"
-          width={500}
-          height={500}
-          className="h-full w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-tl-[30px] md:h-[calc(100%_-_4rem)] md:rounded-tl-[60px]"
-        />
-      </section>
-
-      <section className="overflow-hidden sm:grid sm:grid-cols-2 sm:items-center mb-10 md:mb-0">
-        <div className="px-7 md:px-14 py-10 lg:py-24">
-          <div className="mx-auto max-w-xl">
-            <h2 className="title">Sociedad de Jovenes</h2>
-
-            <p className="text-gray-500 text mt-4">
-              Hola a todos los jóvenes! Les invitamos a nuestra Sociedad de
-              Jóvenes de la iglesia Adventista del Séptimo Día, que se celebra{" "}
-              <strong>el 1er y 3er sábado de cada mes a las 2:30 pm.</strong>{" "}
-              Durante nuestra Sociedad de Jóvenes, tendremos la oportunidad de
-              conectarnos con otros jóvenes de nuestra comunidad de fe, aprender
-              más sobre la Biblia y cómo aplicarla a nuestras vidas diarias, y
-              participar en actividades divertidas y enriquecedoras.
-            </p>
-            <p className="text-gray-500 text mt-4">
-              Nos enfocamos en temas que son relevantes para los jóvenes de hoy,
-              y nos aseguramos de crear un ambiente acogedor y amigable para
-              todos. Queremos que se sientan bienvenidos y cómodos en nuestra
-              comunidad de jóvenes.
-            </p>
-          </div>
-        </div>
-
-        <Image
-          src="/image/jovenes.jpeg"
-          alt="adoracion"
-          width={500}
-          height={500}
-          className="h-full w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-tl-[30px] md:h-[calc(100%_-_4rem)] md:rounded-tl-[60px]"
-        />
-      </section>
-
-      <section className="overflow-hidden sm:grid sm:grid-cols-2 sm:items-center items-start mb-10 md:mb-0">
-        <div className="px-7 md:px-14 py-10 lg:py-24">
-          <div className="mx-auto max-w-xl">
-            <h2 className="title">Banco de alimentos</h2>
-            <p className="text-gray-500 text mt-4">
-              ¡Hola a todos! Les invitamos a nuestro Banco de Alimentos de la
-              iglesia Lake Buena Vista, que se celebra todos los{" "}
-              <strong>jueves a las 3:00 pm.</strong> El Banco de Alimentos es un
-              programa diseñado para proporcionar alimentos y nos enorgullece
-              poder servir a nuestra comunidad de esta manera y esperamos poder
-              ayudar a quienes lo necesitan.
-            </p>
-          </div>
-        </div>
-
-        <Image
-          src="/image/alimentos.jpg"
-          alt="adoracion"
-          width={500}
-          height={500}
-          className="h-full w-full object-cover sm:h-[calc(100%_-_5rem)] sm:self-end sm:rounded-tl-[30px] md:h-[calc(100%_-_10rem)] md:rounded-tl-[60px]"
-        />
-      </section>
+      <article className="px-7 py-12 md:px-8">
+        <p className="text my-4">
+          Bienvenido a nuestra comunidad de la Iglesia Adventista del Séptimo
+          Día Lake Buena Vista. Aquí te invitamos a explorar los diversos
+          servicios y actividades que nuestra iglesia ofrece para enriquecer tu
+          vida espiritual y fortalecer tu conexión con la comunidad.
+        </p>
+        <p className="text my-4">
+          Descubre una amplia gama de oportunidades para crecimiento espiritual,
+          desde nuestros servicios de adoración semanales hasta grupos de
+          estudio bíblico, eventos especiales y programas de servicio
+          comunitario. En Lake Buena Vista, nos esforzamos por proporcionar un
+          ambiente acogedor donde personas de todas las edades y orígenes puedan
+          encontrar inspiración, compañerismo y apoyo en su viaje espiritual.
+        </p>
+        <p className="text my-4">
+          Descubre cómo puedes participar y contribuir al bienestar de nuestra
+          comunidad y más allá. Ya seas un miembro de largo tiempo o estés
+          buscando un lugar para comenzar tu viaje espiritual, te invitamos a
+          ser parte de la familia de Lake Buena Vista.
+        </p>
+      </article>
+      <div className="w-full gap-2 grid grid-cols-12 grid-rows-3 px-7 pb-7 md:px-8">
+        <Card className="col-span-12 sm:col-span-4 md:row-span-2">
+          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            <h4 className="text-white font-medium text-large">
+              Banco de alimentos
+            </h4>
+          </CardHeader>
+          <ImageUI
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://res.cloudinary.com/dseqdhfur/image/upload/v1709579748/comida_13bc18a6cf.jpg"
+          />
+          <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-100 flex justify-end">
+            <Button
+              radius="full"
+              size="sm"
+              as={Link}
+              href="/servicios/banco-de-alimentos"
+            >
+              Saber más
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className="col-span-12 sm:col-span-8 h-[300px]">
+          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            <h4 className="text-white font-medium text-large">Culto Divino</h4>
+          </CardHeader>
+          <ImageUI
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://res.cloudinary.com/dseqdhfur/image/upload/v1709603872/servicios_b2424147b9.jpg"
+          />
+          <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-100 flex justify-end">
+            <Button
+              radius="full"
+              size="sm"
+              as={Link}
+              href="/servicios/culto-divino"
+            >
+              Saber más
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className="col-span-12 sm:col-span-6 md:col-span-4 h-[300px]">
+          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            <h4 className="text-white font-medium text-large">
+              Miércoles de Adoración
+            </h4>
+          </CardHeader>
+          <ImageUI
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://res.cloudinary.com/dseqdhfur/image/upload/v1709344622/tim_wildsmith_ZV_Ncjpfzi94_unsplash_d349592bdb.jpg"
+          />
+          <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-100 flex justify-end">
+            <Button
+              radius="full"
+              size="sm"
+              as={Link}
+              href="/servicios/servicio-de-adoracion"
+            >
+              Saber más
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className="col-span-12 sm:col-span-6 md:col-span-4 h-[300px]">
+          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            <h4 className="text-white font-medium text-large">
+              Escuela Sabática
+            </h4>
+          </CardHeader>
+          <ImageUI
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://res.cloudinary.com/dseqdhfur/image/upload/v1709602786/escuela2_434a970b16.jpg"
+          />
+          <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-100 flex justify-end">
+            <Button
+              radius="full"
+              size="sm"
+              as={Link}
+              href="/servicios/escuela-sabatica"
+            >
+              Saber más
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className="col-span-12 sm:col-span-8 h-[300px]">
+          <ImageUI
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://res.cloudinary.com/dseqdhfur/image/upload/v1711168574/PHOTO_2024_03_22_21_40_03_3e791d30f8.jpg"
+          />
+          <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-100 flex justify-end">
+            <Button
+              radius="full"
+              size="sm"
+              as={Link}
+              href="/servicios/banco-de-alimentos"
+            >
+              Saber más
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className="col-span-12 sm:col-span-4 h-[300px]">
+          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            <h4 className="text-white font-medium text-large">
+              Sociedad de jovenes
+            </h4>
+          </CardHeader>
+          <ImageUI
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://res.cloudinary.com/dseqdhfur/image/upload/v1709688319/escuela_acaf5e3e33.jpg"
+          />
+          <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-100 flex justify-end">
+            <Button
+              radius="full"
+              size="sm"
+              as={Link}
+              href="/servicios/sociedad-de-jovenes"
+            >
+              Saber más
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
     </SectionName>
   )
 }
